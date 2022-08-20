67b5ca86635eadce15e045b9c8ddc918fba0ed90
TedEngineer <tedvu184@gmail.com>
2022-08-20 21:29:26 +1000

    Connect store and postlist component

---
+++ redux-async-actions/src/PostList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "./actions";
+import { useEffect } from "react";
+
+const PostList = () => {
+  return <div></div>;
+};
+
+export default connect(null, { fetchPosts })(PostList);
