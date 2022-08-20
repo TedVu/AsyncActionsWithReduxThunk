b3c4089ff8f85a53ad3248654ccb6e404294cfbc
TedEngineer <tedvu184@gmail.com>
2022-08-20 22:42:27 +1000

    Apply ES6 for action creator

--- redux-async-actions/src/actions/index.js
+++ redux-async-actions/src/actions/index.js
@@ -2,10 +2,9 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 // Action creator must return an action object
 // and action object must be an object with type property and optional payload property
-export const fetchPosts = () => {
-  return async function (dispatch, getState) {
-    const response = await jsonPlaceholder.get("/posts");
+// Function returns a function
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
