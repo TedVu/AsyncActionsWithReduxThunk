4ad98d2de4721f381f21739a46e0cd77db1a4a87
TedEngineer <tedvu184@gmail.com>
2022-08-20 16:30:35 +1000

    Implement store and dummy reducers

--- redux-async-actions/src/index.js
+++ redux-async-actions/src/index.js
@@ -1,15 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { createStore } from "redux";
 import reducers from "./reducers/reducers";
 
-import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
-  <Provider store={configureStore(reducers)}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
+root.render(
+  <React.StrictMode>
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
